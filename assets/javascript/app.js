62df4d3ec34f069b09d835ccb75b7133e87b5b0a
Ravi Joshi <ravipiyushjoshi@gmail.com>
2019-07-11 16:45:31 -0400

    updated app.js with basic structure of functions and variable declaration

---
+++ assets/javascript/app.js
@@ -0,0 +1,39 @@
+var isGameOn = false;
+var istimer = "";
+var questions = [];
+var queAnswers=[];
+var queOptions=[];
+
+function setpage(){
+    console.log("page is set up");
+}
+function timerStart(){
+
+    console.log("game is started");
+}
+function timerStop(){
+
+}
+function gameStart(game){
+    isGameOn =true;
+    
+    timerStart();
+    
+}
+function gameStop(){
+    timerStop();
+}
+function displayQuestions(){
+
+}
+function displayResult(){
+
+}
+
+
+$(document).ready(function(){
+ 
+    
+
+});
+
