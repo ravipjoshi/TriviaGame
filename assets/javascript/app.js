2d7bef5487ca6c76b79783715404c8c7ab6c57f0
Ravi Joshi <ravipiyushjoshi@gmail.com>
2019-07-11 17:18:10 -0400

    Updated code for timer for the game with 2 functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,40 @@
 var isGameOn = false;
-var istimer = "";
+var timer;
+var timerCounter = 30;
 var questions = [];
 var queAnswers=[];
 var queOptions=[];
+var div = $("<div>");
 
+
+function timerDecCounter()
+{ 
+    timerCounter--;
+    $(".time").text(timerCounter);
+}
 function setpage(){
     console.log("page is set up");
 }
 function timerStart(){
+    clearInterval(timer);
+    timer = setInterval(timerDecCounter,1000)
 
+    
     console.log("game is started");
+    if()
 }
 function timerStop(){
+    clearInterval(timer);
 
 }
 function gameStart(game){
     isGameOn =true;
+    div.addClass("timerLbl");
+    div.text("Time Remaining");
+    $("#questionList").append(div);
+    div = $("<div>").addClass("time");
+    div.text(timerCounter);
+    $("#questionList").append(div);
     
     timerStart();
     
@@ -37,3 +56,8 @@ $(document).ready(function(){
 
 });
 
+/*
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+  <label class="form-check-label" for="inlineRadio1">1</label>
+</div>*/
